d65321f81d187f771ea588d9fab5fe2c15e695ed
David Oliveira <davidonshipping@gmail.com>
2022-07-03 00:29:21 -0300

    cria metodo carencia conceito polimorfismo

--- assets/financiamentocarencia.js
+++ assets/financiamentocarencia.js
@@ -8,5 +8,16 @@ class FinanciamentoCarencia extends Financiamento {
   constructor(valor,entrada,taxaJuros,prazo,carencia) {
     super(valor,entrada,taxaJuros,prazo);
     this.#taxaJuros = taxaJuros;
+    this.#parcelas = super.getParcelas();
+    this.#carencia = carencia;
+  }
+
+  calcParcelasMensais() { //polimorfismo
+    let saldo = this.#parcelas[0].getSaldo(); //posição zero == saldo devedor
+    for(let i=0; i < this.#carencia; i++) {
+      const numero = this.#parcelas.length;
+      saldo += Financiamento.calcJuros(saldo,this.#taxaJuros);
+      this.#parcelas.push(new Parcela(0,0,0,saldo));
+    }
   }
 }

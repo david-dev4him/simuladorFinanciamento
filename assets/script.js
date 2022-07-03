7a5700eec1c7d68024231b95ba5387da55c1f557
David Oliveira <davidonshipping@gmail.com>
2022-07-02 21:56:18 -0300

    adiciona ação botão calcular

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
 const comCarencia = document.querySelector('#comCarencia');
 const listaSuspensa = document.querySelector('#listaSuspensa');
 const corpoTabela = document.querySelector('#corpoTabela');
+const botaoCalcular = document.querySelector('#botaoCalular');
+const textoValor = document.querySelector('#textoValor');
+const textoEntrada = document.querySelector('#textoEntrada');
+const textoTaxaJuros = document.querySelector('#textoTaxaJuros');
+const textoPrazo = document.querySelector('#textoPrazo');
 
 comCarencia.addEventListener('change',function() {
   if(this.checked){
@@ -8,4 +13,13 @@ comCarencia.addEventListener('change',function() {
   } else {
     listaSuspensa.setAttribute('hidden','hidden');
   }
+});
+
+botaoCalcular.addEventListener('click',function() {
+  const valor = parseFloat(textoValor.value);
+  const entrada = parseFloat(textoEntrada.value);
+  const taxaJuros = parseFloat(textoTaxaJuros.value);
+  const prazo = parseFloat(textoPrazo.value);
+  let simulacao;
+  
 });

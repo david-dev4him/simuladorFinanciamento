c7acf959a616059d6c4d64ab281b757ca16238f9
David Oliveira <davidonshipping@gmail.com>
2022-07-02 21:17:55 -0300

    atualizar lógica exibição de parcelas

--- assets/financiamento.js
+++ assets/financiamento.js
@@ -33,6 +33,10 @@ class Financiamento {
     const parcelas = this.#parcelas.slice(1);
     for(const parcela of parcelas) {
       const linha = corpoTabela.insertRow(-1);
+      for (const dado of parcela.getDadosFormatados()) {
+        const celula = linha.insertCell(-1);
+        celula.textContent = dado;
+      }
     }
   }
 }
